7755f487db85dc38dc70348654088068718a45f9
lisa2305 <117445066+lisa2305@users.noreply.github.com>
2023-03-17 11:34:08 +0100

    2 files modified

--- src/app/localita/lista-localita.component.ts
+++ src/app/localita/lista-localita.component.ts
@@ -7,7 +7,7 @@ export const LOCALITA: Localita[] = [
     immagine:
       'https://www.viaggimust.it/wp-content/uploads/2020/04/lago-di-garda-cosa-vedere-1.jpg',
     prezzo: 158,
-    starRating: 2.5,
+    starRating: 3.5,
   },
   {
     name: 'Lago di Ledro',
@@ -15,7 +15,7 @@ export const LOCALITA: Localita[] = [
     immagine:
       'https://www.trentino.com/images/cms/main/754x435/B-8365-lago-di-ledro-ledrosee.jpg',
     prezzo: 150.99,
-    starRating: 4.8,
+    starRating: 2.5,
   },
   {
     name: 'Lago di Caldonazzo',
@@ -23,6 +23,6 @@ export const LOCALITA: Localita[] = [
     immagine:
       'https://hotelmicamada.it/attachment/get/up_24_1365434315_826x466.jpg',
     prezzo: 982,
-    starRating: 5,
+    starRating: 4,
   },
 ];
